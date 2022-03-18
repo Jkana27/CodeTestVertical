c95c4ae3f8aa8afe805648020b2230fc711de34d
JacobKana <jacob_kana@yahoo.com>
2022-03-17 20:33:00 -0500

    Add project files.

---
+++ CodeTestVertical/Models/WidgetModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeTestVertical.Models
+{
+    public class WidgetModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Minimum length is {2}")]
+        [Display(Name = "Widget Name")]
+        public string WidgetName { get; set; }
+        [Required]
+        [Display(Name = "Widget Type")]
+        public string WidgetType { get; set; }
+        [Required]
+        [Display(Name = "Widget Creation Date and Time")]
+        public DateTime WidgetCreation { get; set; }
+        [Required]
+        [Display(Name = "Widget Sub-Type")]
+        public string WidgetSubType { get; set; }
+
+
+    }
+}
---
+++ CodeTestVertical/wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let subTypes = {
+    "A": ["apple", "Aardvark", "Astronaut"],
+    "B": ["Bear", "Bobcat", "Beaver"],
+    "one": ["First", "One", "Primary", "Uno"],
+    "two": ["Second","Two","Secondary","Dos"]
+}
+
+
+function dropDown(value) {
+    let widgetSub = document.getElementById("SubType");
+    widgetSub.innerHTML = "";
+    if (value == "A") {
+        subTypes.A.forEach(sub => {
+            let option = document.createElement("option");
+            option.textContent = sub;
+            widgetSub.appendChild(option);
+
+        });
+    }
+    if (value == "B") {
+        subTypes.B.forEach(sub => {
+            let option = document.createElement("option");
+            option.textContent = sub;
+            widgetSub.appendChild(option);
+
+        });
+    }
+    if (value == "1") {
+        subTypes.one.forEach(sub => {
+            let option = document.createElement("option");
+            option.textContent = sub;
+            widgetSub.appendChild(option);
+
+        });
+    }
+    if (value == "2") {
+        subTypes.two.forEach(sub => {
+            let option = document.createElement("option");
+            option.textContent = sub;
+            widgetSub.appendChild(option);
+
+        });
+    }
+}
+
+function applyDropDown(){
+    //get value of type of Widget
+    let Type = document.getElementById("WidgetType").value;
+    dropDown(Type);
+}
+
+document.getElementById("WidgetType").addEventListener("change", applyDropDown);
+document.addEventListener("DOMContentLoaded", applyDropDown);
